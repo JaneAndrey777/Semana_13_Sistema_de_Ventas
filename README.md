d09518e1c1adaa909e9029d5e3e0b0598fc79cb4
JaneAndrey777 <j.mendez.blanco26@gmail.com>
2025-12-04 19:56:54 -0600

    Clase 13

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Semana_13_Sistema_de_Ventas
 Repaso final - Curso de Python
+# Autor Jane Andrey Méndez Blanco 
+### Fecha Dec 4 2025
+## Versión 1.0
---
+++ app.py
@@ -0,0 +1,12 @@
+
+import pandas as pd
+import os
+
+
+from datos import guardar_csv
+
+#la lista que funciona como base de datos
+ventas_data = []
+guardar_csv(ventas_data)
+
+print('Cargando datos de ventas desde ventas csv...')
